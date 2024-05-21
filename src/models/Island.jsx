c9a02d1cb47362bf46b014bb2945fa695c24713e
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-05-21 12:52:46 +0530

    .

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -33,6 +33,9 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
     setIsRotating(false);
     const clientX = e.touches ? e.touches[0].clientX : e.clientX;
     const delta = (clientX - lastX.current) / viewport.width;
+    islandRef.current.rotaion.y += delta * 0.01 * Math.PI;
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
   };
   const handlePointerMove = (e) => {
     e.stopProgation();
