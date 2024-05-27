d9a3f71dec8c9c9965a6db4d9e8c3d6f6e8a8101
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-05-27 15:38:42 +0530

    .

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Tarun
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Frontend Developer based in India, specializing in technical education
+          through hands-on learning and building web applications.
+        </p>
+      </div>
+      <div className="py-10 flex flex-col">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap gap-12"></div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
